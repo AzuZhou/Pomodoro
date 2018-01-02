039e9dc14425b4db43521384b0a25193f660888c
Azu <abbey-1825@hotmail.com>
2018-01-02 01:45:22 -0300

    Initial Commit

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    let changeSeconds = function () {
+        let seconds = 59;
+        document.getElementById("seconds").innerHTML = seconds;
+    };
+
+    document.querySelector("#trigger").onclick = function () { setTimeout(changeSeconds, 1000) };
+})
