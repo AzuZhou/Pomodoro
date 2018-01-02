32da99b5e70bb836145153e05e3876c1d173ddf6
Azu <abbey-1825@hotmail.com>
2018-01-02 01:10:33 -0300

    Removing the wrong main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-
-    let changeSeconds = function () {
-        let seconds = 59;
-        document.getElementById("seconds").innerHTML = seconds;
-    };
-
-    document.querySelector("#trigger").onclick = function () { setTimeout(changeSeconds, 1000) };
-})
