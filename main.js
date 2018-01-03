bae386f692a013c135b9cd9f1f30a8a173fb58d5
Azu <abbey-1825@hotmail.com>
2018-01-02 23:58:50 -0300

    Timer working

--- main.js
+++ main.js
@@ -1,9 +1,44 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    let changeSeconds = function () {
+    let interval = function () {
+
+        let minutes = Number(document.getElementById("minutes").innerHTML) - 1;
         let seconds = 59;
-        document.getElementById("seconds").innerHTML = seconds;
+
+        setTimeout(function () {
+            document.getElementById("minutes").innerHTML = minutes;
+        }, 1000)
+
+        setInterval(function () {
+
+            if (minutes > 0) {
+
+                if (seconds > 0) {
+                    document.getElementById("seconds").innerHTML = seconds--;
+                    console.log(seconds)
+                } else if (seconds == 0) {
+                    document.getElementById("seconds").innerHTML = seconds;
+                    seconds--;
+                    document.getElementById("minutes").innerHTML = minutes--;
+                } else {
+                    document.getElementById("minutes").innerHTML = minutes--;
+                    seconds = 59;
+                }
+
+            } else if (minutes == 0) {
+
+                document.getElementById("minutes").innerHTML = minutes;
+                (seconds < 0) ? clearInterval(interval) : document.getElementById("seconds").innerHTML = seconds--;
+
+            }
+
+        }
+            , 1000)
+
     };
 
-    document.querySelector("#trigger").onclick = function () { setTimeout(changeSeconds, 1000) };
+
+    let start = document.getElementById("trigger").addEventListener('click', interval);
+
 })
+
