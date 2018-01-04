e33a2e242aac925288f695e840c38da852048049
Azu <abbey-1825@hotmail.com>
2018-01-03 22:29:15 -0300

    Stopping timer

--- main.js
+++ main.js
@@ -1,42 +1,63 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+    let sessionStarted = false;
+    let pause = false;
+    let resume = false;
+    let lastMin;
+    let lastSec;
+    let minutes = Number(document.getElementById("minutes").innerHTML) - 1;
+    let seconds = 60;
+
     let interval = function () {
 
-        let minutes = Number(document.getElementById("minutes").innerHTML) - 1;
-        let seconds = 60;
-
         setTimeout(function () {
-            document.getElementById("minutes").innerHTML = minutes;
+            return document.getElementById("minutes").innerHTML = minutes;
         }, 1000)
 
-        setInterval(function () {
+        let currentInterval = setInterval(function () {
 
-            if (minutes > 0) {
-
-                if (seconds > 0) {
-                    seconds--;
-                    document.getElementById("seconds").innerHTML = seconds;
-                } else {
-                    seconds = 59;
-                    minutes--;
-                    document.getElementById("minutes").innerHTML = minutes;
-                    document.getElementById("seconds").innerHTML = seconds;
+            if (pause) {
+                clearInterval(currentInterval);
+            } else {
+                if (minutes > 0) {
+                    if (seconds > 0) {
+                        seconds--;
+                        lastSec = seconds;
+                    } else {
+                        seconds = 59;
+                        minutes--;
+                    }
+                } else if (minutes == 0) { //interval
+                    (seconds < 0) ? clearInterval(currentInterval) : document.getElementById("seconds").innerHTML = seconds--;
                 }
-
-            } else if (minutes == 0) {
-
-                document.getElementById("minutes").innerHTML = minutes;
-                (seconds < 0) ? clearInterval(interval) : document.getElementById("seconds").innerHTML = seconds--;
-
             }
 
+            lastMin = minutes;
+            lastSec = seconds;
+            document.getElementById("seconds").innerHTML = lastSec;
+            document.getElementById("minutes").innerHTML = lastMin;
         }
-            , 1000)
-
+            , 1000);
     };
 
+    /*let newInterval = function () {
 
-    let start = document.getElementById("trigger").addEventListener('click', interval);
+    };*/
+
+    let startSession = function () {
+
+        if (sessionStarted) {
+            pause = true;
+            interval();
+            /*if (resume) {
+
+            }*/
+        } else {
+            sessionStarted = true;
+            interval();
+        }
+    };
+
+    let start = document.getElementById("trigger").addEventListener('click', startSession);
 
 })
-
