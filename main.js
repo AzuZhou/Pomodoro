58dcc935f9e63a043b4156c1d846ae7e1a02757f
Azu <abbey-1825@hotmail.com>
2018-01-04 16:41:15 -0300

    Now I can stop the timer

--- main.js
+++ main.js
@@ -2,11 +2,44 @@ document.addEventListener('DOMContentLoaded', function () {
 
     let sessionStarted = false;
     let pause = false;
-    let resume = false;
-    let lastMin;
-    let lastSec;
     let minutes = Number(document.getElementById("minutes").innerHTML) - 1;
     let seconds = 60;
+    let lastMin;
+    let lastSec;
+    let min;
+    let sec;
+    let counter = 1;
+
+    let clickCounter = function () {
+        counter++;
+    }
+
+    let resume = function () {
+        if (counter % 2 != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    let showTime = function (lastMin, lastSec) {
+        sec = lastSec.toString();
+        min = lastMin.toString();
+
+        if (sec.length == 1) {
+            sec = "0" + sec;
+            document.getElementById("seconds").innerHTML = sec;
+        } else {
+            document.getElementById("seconds").innerHTML = lastSec;
+        }
+
+        if (min.length == 1) {
+            min = "0" + min;
+            document.getElementById("minutes").innerHTML = min;
+        } else {
+            document.getElementById("minutes").innerHTML = lastMin;
+        }
+    }
 
     let interval = function () {
 
@@ -22,36 +55,65 @@ document.addEventListener('DOMContentLoaded', function () {
                 if (minutes > 0) {
                     if (seconds > 0) {
                         seconds--;
-                        lastSec = seconds;
                     } else {
                         seconds = 59;
                         minutes--;
                     }
-                } else if (minutes == 0) { //interval
+                } else if (minutes == 0) {
                     (seconds < 0) ? clearInterval(currentInterval) : document.getElementById("seconds").innerHTML = seconds--;
                 }
             }
 
             lastMin = minutes;
             lastSec = seconds;
-            document.getElementById("seconds").innerHTML = lastSec;
-            document.getElementById("minutes").innerHTML = lastMin;
+
+            showTime(lastMin, lastSec);
+
         }
             , 1000);
     };
 
-    /*let newInterval = function () {
+    let newInterval = function () {
+        lastMin = Number(document.getElementById("minutes").innerHTML);
+        lastSec = Number(document.getElementById("seconds").innerHTML);
+        minutes = lastMin;
+        seconds = lastSec;
 
-    };*/
+        let otherInterval = setInterval(function () {
+
+            if (pause) {
+                clearInterval(otherInterval);
+            } else {
+                if (minutes > 0) {
+                    if (seconds > 0) {
+                        seconds--;
+                    } else {
+                        seconds = 59;
+                        minutes--;
+                    }
+                } else if (minutes == 0) {
+                    (seconds < 0) ? clearInterval(currentInterval) : document.getElementById("seconds").innerHTML = seconds--;
+                }
+            }
+
+            lastMin = minutes;
+            lastSec = seconds;
+
+            showTime(lastMin, lastSec);
+
+        }, 1000)
+    };
 
     let startSession = function () {
 
         if (sessionStarted) {
-            pause = true;
-            interval();
-            /*if (resume) {
-
-            }*/
+            if (resume()) {
+                pause = false;
+                newInterval();
+            } else {
+                pause = true;
+                newInterval();
+            }
         } else {
             sessionStarted = true;
             interval();
@@ -59,5 +121,9 @@ document.addEventListener('DOMContentLoaded', function () {
     };
 
     let start = document.getElementById("trigger").addEventListener('click', startSession);
+    let count = document.getElementById("trigger").addEventListener('click', clickCounter);
 
 })
+
+/*
+    */
